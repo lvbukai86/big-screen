2761536e0cd35e45a5fe5efa35fc416dbc5036e5
ada <304368809@qq.com>
2022-02-14 22:02:09 +0800

    重构chart1

--- src/pages/home.tsx
+++ src/pages/home.tsx
@@ -1,10 +1,21 @@
-import React from 'react';
+import React, {useEffect, useRef} from 'react';
 import './home.scss';
+import headerBg from '../images/header.png';
+import {Chart1} from '../components/chart-1';
 
 export const Home = () => {
-  return (
-    <div>
-      你好
-    </div>
-  );
+    return (
+        <div className="home">
+            <header style={{backgroundImage: `url(${headerBg})`}}/>
+            <main>
+                <section className="section1">
+                    <Chart1/>
+                </section>
+                <section className="bordered section2"></section>
+                <section className="bordered section3"></section>
+                <section className="bordered section4"></section>
+                <section className="bordered section5"></section>
+            </main>
+        </div>
+    );
 };
